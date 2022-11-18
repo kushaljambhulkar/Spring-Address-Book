c95b1b044b616f3d3f21bd53e82b1cd53733c302
Kushal Jambhulkar <kushaljambhulkar82@gmail.com>
2022-11-18 19:11:07 +0530

    Add:Complete Address Book with All CRUD opretion

--- src/main/java/com/example/springaddressbook/service/AddressBookServiceI.java
+++ src/main/java/com/example/springaddressbook/service/AddressBookServiceI.java
@@ -7,21 +7,18 @@ import java.util.List;
 import java.util.Optional;
 
 public interface AddressBookServiceI {
+    //Method to insert Data In the DataBase
     AddressBook addData(AddressBookDTO addressBookDTO);
-
+    //Get ID from AddressBook
     Optional<AddressBook> getById(int Id);
-
+    //Get find person by firstName
     List<AddressBook> findByFirstName(String firstName);
-
-    List<AddressBook> findByCity(String city);
-
+    //Find person by Zipcode using Custom Query in Repository
     List<AddressBook> orderByZipcode();
-
+    //Get all the data of AddressBook from DataBase
     List<AddressBook> getData();
-
+    //Update the details of person in DataBase
     AddressBook updateData(AddressBookDTO addressBookDTO, int Id);
-
+    //Delete Data By using Id
     List<AddressBook> deleteById(int ID);
-
-    List<AddressBook> deleteAll();
 }
